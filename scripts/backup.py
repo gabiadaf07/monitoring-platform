471e76726490d4c7fd7e5256870aa46224074787
gabiadaf07 <adafineigabriel07@gmail.com>
2025-10-25 15:18:41 +0300

    Am adaugat un test pentru verificare continut

--- scripts/backup.py
+++ scripts/backup.py
@@ -9,36 +9,37 @@ interval_backup = int(os.getenv("INTERVAL_BACKUP", "5"))
 backup_dir = os.getenv("BACKUP_FOLDER", "./backups")
 log_path = os.getenv("LOG_FOLDER", "./logs/system-state.log")
 state_file = '.last_mtime'
-
+hash_file = '.last_hash'
 
 def get_hash_file(path):
     with open(path, 'rb') as f:
         return hashlib.sha256(f.read()).hexdigest()
-    
+
 def back_up_fisier():
     if not os.path.exists(log_path):
         print("Fișierul system-state.log nu există!")
         return
 
-    
-
-    #Getting the time using datetime
     os.makedirs(backup_dir, exist_ok=True)
     current_mtime = os.path.getmtime(log_path)
     mtime_datetime = datetime.fromtimestamp(current_mtime)
     formatted_time = mtime_datetime.strftime("%Y-%m-%d %H:%M")
     print(f"Fișierul a fost modificat la: {formatted_time}")
 
-    current_hash = get_hash_file("/home/admin0103/source/monitoring-platform/scripts/logs/system-state.log")
-    future_hash = get_hash_file("/home/admin0103/source/monitoring-platform/scripts/logs/system-state.log")
+    current_hash = get_hash_file(log_path)
 
-    
     last_mtime = 0
+    last_hash = ""
+
     if os.path.exists(state_file):
         with open(state_file, 'r') as f:
             last_mtime = float(f.read())
 
-    if current_mtime != last_mtime and current_hash != future_hash:
+    if os.path.exists(hash_file):
+        with open(hash_file, 'r') as f:
+            last_hash = f.read().strip()
+
+    if current_mtime != last_mtime and current_hash != last_hash:
         timestamp = mtime_datetime.strftime("%Y%m%d_%H%M")
         backup_filename = f"monitoring_backup_{timestamp}.txt"
         backup_path = os.path.join(backup_dir, backup_filename)
@@ -48,9 +49,10 @@ def back_up_fisier():
 
         with open(state_file, 'w') as f:
             f.write(str(current_mtime))
+        with open(hash_file, 'w') as f:
+            f.write(current_hash)
     else:
         print("Fișierul nu a fost modificat.")
-
 
 while True:
     try:
